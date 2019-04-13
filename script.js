017ca2ef2a864228cc23fd6cbd30c1edf3fe6890
Tony Cruz <tonycruz@Tonys-MBP.frontierlocal.net>
2019-04-13 16:26:10 -0500

    added some styling to the page

--- script.js
+++ script.js
@@ -2,32 +2,49 @@ var cardHead = document.getElementById("card");
 var submitChoice = document.getElementById("choice");
 var userText = document.getElementById("textField");
 var parentButton = document.getElementById("buttons");
-
+var parentImg = document.querySelector(".column");
 
 
 var celeb = ["Kanye West", "Drake", "Adam Sandler", "Post Malone", "The Beatles", "Led Zeppelin", "Aerosmith", "Red Hot Chili Peppers"];
 
 submitChoice.addEventListener("click", createButton);
 
+document.getElementById("displayAll").addEventListener("click", function(e) {
+    if (e.target.nodeName === "IMG") {
+        var currentSrc = e.target.src;
+        var playGif = currentSrc.replace("480w_s.jpg", "giphy.gif");
+        e.target.src = playGif;
+    } 
+    // else if (e.target.src === playGif) {
+    //     var pause = currentSrc.replace("giphy.gif", "480w_s.jpg");
+    //     e.target.src = pause;
+    //     console.log("it is working");
+    // }
+})
+
 parentButton.addEventListener("click", function(e) {
     if (e.target.nodeName === "BUTTON") {
         var catergory = e.target.value;
-
+        
         console.log(catergory);
         $.ajax({
             url:"https://api.giphy.com/v1/gifs/search?api_key=NsMqXegVVkAHkKcMrCFJG79H12Qxtdas&q=" + catergory + "&limit=10&offset=0&rating=R&lang=en",
             method: "GET"
         }).then(function(response) {
+            console.log(response);
             for (var i = 0; i < response.data[i].images.fixed_width.url.length; i++) {
                 var gifHold = document.createElement("div");
                 gifHold.className = "column";
                 var imageLink = response.data[i].images.fixed_width.url;
+                var static = imageLink.replace("200w.gif", "480w_s.jpg");
+                console.log(static);
 
-                var textCard = $("<p>").text("Rating: ");
+                var textCard = $("<p>");
+                textCard.text("Rating: ");
                 gifHold.append(textCard);
 
                 var newImg = document.createElement("img");
-                newImg.src = imageLink;
+                newImg.src = static;
                 gifHold.append(newImg);
 
                 $("#displayAll").append(gifHold);
